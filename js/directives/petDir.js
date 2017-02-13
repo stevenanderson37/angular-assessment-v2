a0fb3ce541503cfc24263048ecbe81ff6aeeabbc
Steven Anderson <happygoatdesigns@gmail.com>
2017-02-13 15:01:26 -0700

    completed with the click section of step 5 now working strictly with angular and no jQuery and still under 3 hours

--- js/directives/petDir.js
+++ js/directives/petDir.js
@@ -3,33 +3,33 @@ angular.module("assessment")
 
   return {
     restrict: 'AE',
-    templateUrl: './views/pet-tmpl.html',
-    controller: function($scope, mainService, $state, $rootScope) {
-
-      $(document).ready(function() {
-        $("#"+$scope.pet.id).click(function() {
-          var clicks = $(this).data("clicks");
-          if (clicks) {
-            // odd clicks
-            $(".image"+$scope.pet.id).css({
-              "display":"block"
-            });
-            $(".showimage"+$scope.pet.id).css({
-              "display":"none"
-            });
-          } else {
-            // even clicks
-            $(".image"+$scope.pet.id).css({
-              "display":"none"
-            });
-            $(".showimage"+$scope.pet.id).css({
-              "display":"block"
-            });
-          }
-          $(this).data("clicks", !clicks);
-        });
-      });
-    }
+    templateUrl: './views/pet-tmpl.html'
+    // controller: function($scope, mainService, $state, $rootScope) {
+    //
+    //   $(document).ready(function() {
+    //     $("#"+$scope.pet.id).click(function() {
+    //       var clicks = $(this).data("clicks");
+    //       if (clicks) {
+    //         // odd clicks
+    //         $(".image"+$scope.pet.id).css({
+    //           "display":"block"
+    //         });
+    //         $(".showimage"+$scope.pet.id).css({
+    //           "display":"none"
+    //         });
+    //       } else {
+    //         // even clicks
+    //         $(".image"+$scope.pet.id).css({
+    //           "display":"none"
+    //         });
+    //         $(".showimage"+$scope.pet.id).css({
+    //           "display":"block"
+    //         });
+    //       }
+    //       $(this).data("clicks", !clicks);
+    //     });
+    //   });
+    // }
   }
 
 
